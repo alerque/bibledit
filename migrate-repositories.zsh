a0352e49e9c1e6f99d1cd4446b4f9bff1e136536
Caleb Maclennan <caleb@alerque.com>
2016-01-28 18:40:35 +0200

    Start script to split out one repository per platform

---
+++ migrate-repositories.zsh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+
+set -e
+
+BASE=~/projects/bibledit
+TARGET=~/scratch/bibledit
+
+function show-authors() {
+	git log --format='%an <%ae>' | sort | uniq -c | sort -n
+}
+
+function normalize-authors () {
+	show-authors
+	git filter-branch -f --commit-filter '
+		if  [[ $GIT_AUTHOR_EMAIL =~ teus* ]] || [[ $GIT_AUTHOR_EMAIL =~ translation* ]]; then
+			GIT_AUTHOR_NAME="Teus Benschop";
+			GIT_AUTHOR_EMAIL="teusjannette@gmail.com";
+		fi;
+		if  [[ $GIT_AUTHOR_EMAIL =~ dand* ]]; then
+			GIT_AUTHOR_NAME="Dan Dennison";
+			GIT_AUTHOR_EMAIL="dand@thedennisons.org";
+		fi;
+		GIT_COMMITTER_NAME="Caleb Maclennan";
+		GIT_COMMITTER_EMAIL="caleb@alerque.com";
+		git commit-tree "$@";
+	'
+	show-authors
+}
+
+pushd $TARGET
+
+[[ -d bibledit-core ]] || git clone --branch=master $BASE bibledit-core
+pushd bibledit-core
+normalize-authors
+git log -1
+popd
+
+ls -al
