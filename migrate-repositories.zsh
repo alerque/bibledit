cb9191ebdb3ce0f0534c1f6dc7621d78b30d559b
Caleb Maclennan <caleb@alerque.com>
2016-01-29 11:24:28 +0200

    Commit filter-branches as current user

--- migrate-repositories.zsh
+++ migrate-repositories.zsh
@@ -11,23 +11,25 @@ function show-authors() {
 }
 
 function normalize-authors () {
-	git filter-branch -f --commit-filter '
-		if  [[ $GIT_AUTHOR_EMAIL =~ teus* ]] || [[ $GIT_AUTHOR_EMAIL =~ translation* ]]; then
-			GIT_AUTHOR_NAME="Teus Benschop";
-			GIT_AUTHOR_EMAIL="teusjannette@gmail.com";
-		fi;
-		if  [[ $GIT_AUTHOR_EMAIL =~ mattias* ]]; then
-			GIT_AUTHOR_NAME="Mattias Põldaru";
-			GIT_AUTHOR_EMAIL="mahfiaz@gmail.com";
-		fi;
-		if  [[ $GIT_AUTHOR_EMAIL =~ dand* ]]; then
-			GIT_AUTHOR_NAME="Dan Dennison";
-			GIT_AUTHOR_EMAIL="dan@thedennisons.org";
-		fi;
-		GIT_COMMITTER_NAME="Caleb Maclennan";
-		GIT_COMMITTER_EMAIL="caleb@alerque.com";
-		git commit-tree "$@";
-	'
+	show-authors | grep -qi Compaq && \
+		git filter-branch -f --commit-filter '
+			if  [[ $GIT_AUTHOR_EMAIL =~ teus* ]] || [[ $GIT_AUTHOR_EMAIL =~ translation* ]]; then
+				GIT_AUTHOR_NAME="Teus Benschop";
+				GIT_AUTHOR_EMAIL="teusjannette@gmail.com";
+			fi;
+			if  [[ $GIT_AUTHOR_EMAIL =~ mattias* ]]; then
+				GIT_AUTHOR_NAME="Mattias Põldaru";
+				GIT_AUTHOR_EMAIL="mahfiaz@gmail.com";
+			fi;
+			if  [[ $GIT_AUTHOR_EMAIL =~ dand* ]]; then
+				GIT_AUTHOR_NAME="Dan Dennison";
+				GIT_AUTHOR_EMAIL="dan@thedennisons.org";
+			fi;
+			GIT_COMMITTER_NAME="$(git config user.name)";
+			GIT_COMMITTER_EMAIL="$(git config user.email)";
+			git commit-tree "$@";
+		'
+	show-authors
 }
 
 #rm -rf $TARGET ; mkdir $TARGET
@@ -35,15 +37,13 @@ pushd $TARGET
 
 [[ -d bibledit-core ]] || git clone --branch=master $BASE bibledit-core
 pushd bibledit-core
-show-authors | grep -q Compaq && normalize-authors
-show-authors
+normalize-authors
 git log -1
 popd
 
 [[ -d bibledit-web ]] || git clone --branch=savannah/bibledit-web $BASE bibledit-web
 pushd bibledit-web
-show-authors | grep -q jurak && normalize-authors
-show-authors
+normalize-authors
 popd
 
 ls -al
